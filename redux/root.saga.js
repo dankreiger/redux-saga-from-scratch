4baca081dec1a18ef81013aa24b23c9d03a0c711
Dan Kreiger <dan@dankreiger.com>
2020-02-26 00:04:27 +0100

    Update root.saga.js

--- redux/root.saga.js
+++ redux/root.saga.js
@@ -1,6 +1,7 @@
 import { fork } from './lib/effects'
 import { woofWatcher, fetchWatcher } from './dog/sagas';
 
+// haven't figured out how to make the all effect yet...
 export function* rootSaga() {
   yield fork(fetchWatcher);
   yield fork(woofWatcher);
