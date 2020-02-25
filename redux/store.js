02d7cba4bb065da5c40ce1bc2740e3740108a30a
Dan Kreiger <dan@dankreiger.com>
2020-02-26 00:10:28 +0100

    chore: bye thunk

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,13 @@
 import { createStore } from "./lib/createStore";
 import applyMiddleware from "./lib/applyMiddleware";
 import logger from './middlewares/logger';
-import thunk from './middlewares/thunk';
 import createPuppyMiddleware from "./middlewares/puppyMiddleware";
 import { rootReducer } from "./root.reducer";
 import { rootSaga } from "./root.saga";
 
 const puppyMiddleware = createPuppyMiddleware();
 
-const middlewares = [logger, thunk, puppyMiddleware];
+const middlewares = [logger, puppyMiddleware];
 
 const preloadedState = undefined;
 const _store = createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
